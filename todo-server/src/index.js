a0dddf4bf7f424ad9cf838bf35caaae8119b42d7
Kicki Halmos <kickihalmos@gmail.com>
2021-09-29 11:35:24 +0200

    setup todo-server with express and mongoose

---
+++ todo-server/src/index.js
@@ -0,0 +1,31 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const mongoUri = 'mongodb+srv://kicki:admin@cluster0.dca84.mongodb.net/todoDB?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+   // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', ()=> {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo', err);
+})
+
+app.get('/', (req,res) => {
+    res.send('Welcome to backend')
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
