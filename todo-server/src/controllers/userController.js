ea491df01d03c16cca81a8045177d352ca6796b5
Kicki Halmos <kickihalmos@gmail.com>
2021-10-05 22:23:52 +0200

    setup login function in userController

--- todo-server/src/controllers/userController.js
+++ todo-server/src/controllers/userController.js
@@ -1,37 +1,68 @@
-const mongoose = require('mongoose');
-const jwt = mongoose.model('jsonwebtoken');
-const User = require('../models/User');
+const mongoose = require("mongoose");
+const jwt = mongoose.model("jsonwebtoken");
+const User = require("../models/User");
 
-const signUp = async (req, res) => {
-const {email, password} = req.body
+const signup = async (req, res) => {
+  const { email, password } = req.body;
 
-if(!email || !password){
+  if (!email || !password) {
     return res
-    .status(400)
-    .jsont({success:false, error: "You must provied email and password"})
-}
+      .status(400)
+      .jsont({ success: false, error: "You must provied email and password" });
+  }
 
-const user = await new User({
+  const user = await new User({
     email,
-    password
-});
+    password,
+  });
 
-user.save()
-.then(() => {
-const token = jwt.sign({userId: user._id}, "My_boli_token");
-return res.status(200).json({
-    success: true,
-    id: user._id,
-    message: 'User created',
-    data: User
-})
-})
-.catch((err)=>{
-    console.log(err);
-    return res.statuse(400).json({
-        success: false,
-        message: 'User not created'
+  user
+    .save()
+    .then(() => {
+      const token = jwt.sign({ userId: user._id }, "My_boli_token");
+      return res.status(200).json({
+        success: true,
+        id: user._id,
+        message: "User created",
+        data: User,
+      });
     })
-})
-}
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({
+        success: false,
+        message: "User not created",
+      });
+    });
+};
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .jsont({ success: false, error: "You must provied email and password" });
+  }
+
+  await User.findOne({ email })
+    .exec()
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ success: false, error: "Invalid password or email" });
+      }
+
+      try{
+          await user.comparePassword(password);
+          const token = jwt.sign({userId: user._id}, 'My_boli_token');
+          res.status(200).json({success:true, data: user, token: token})
+      } catch (err){
+          return res.status(401).send({error: 'Invalid password or email'})
+      }
+    })
+    .catch((err) => {
+        return res.status(400).json({ success: false, error: err });
+      });
+};
