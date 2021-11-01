e5aa7e385aebf15309bef3953775ba754ccca571
Kicki Halmos <kickihalmos@gmail.com>
2021-11-01 13:31:28 +0100

    refactor todo-routes and todo-controller in a better way

--- todo-server/controllers/todoController.js
+++ todo-server/controllers/todoController.js
@@ -3,109 +3,79 @@
 
 const Todo = require('../models/Todo');
 
-const getTodoList = async (req, res) => {
+const getTodoList = async (userId) => {
   // get todolist from mongodb
-  await Todo.find({ userId: req.user._id })
-    .exec()
-    .then((todoList) => {
-      if (!todoList) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Todo-list not found' });
-      }
-      return res.status(200).json({ success: true, data: todoList });
-    })
-    .catch((err) => res.status(400).json({ success: false, error: err }));
+  try {
+    const todoList = await Todo.find({ userId }).exec();
+    return { success: true, data: todoList };
+  } catch (error) {
+    return { success: false, error };
+  }
 };
 
-const getTodoById = async (req, res) => {
+const getTodoById = async (id) => {
   // get single todo by ID from mongodb
-  await Todo.findOne({ _id: req.params.id })
-    .exec()
-    .then((todoItem) => {
-      if (!todoItem) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Todo item not found' });
-      }
-
-      return res.status(200).json({ success: true, data: todoItem });
-    })
-    .catch((err) => res.status(400).json({ success: false, error: err }));
+  try {
+    const todoItem = await Todo.findOne({ _id: id })
+      .exec();
+    return { success: true, data: todoItem };
+  } catch (error) {
+    return { success: false, error };
+  }
 };
 
-const updateTodoItem = async (req, res) => {
+const updateTodoItem = async (id, body) => {
   // update todo item and post to mongodDB
+  if (!body) return { success: false, error: 'You must provide a body to update' };
 
-  const { body } = req;
-  if (!body) {
-    return res
-      .status(400)
-      .json({ success: false, error: 'You must provide a body to update' });
+  try {
+    const updatedItem = await Todo.findByIdAndUpdate(id, {
+      title: body.title,
+      body: body.body,
+    }, { new: true })
+      .exec();
+    return {
+      success: true,
+      data: updatedItem,
+      message: 'Todo item was updated',
+    };
+  } catch (error) {
+    return { success: false, error };
   }
-  await Todo.findByIdAndUpdate(req.params.id, {
-    title: body.title,
-    body: body.body,
-  }, { new: true })
-    .exec()
-    .then((updatedItem) => {
-      if (!updatedItem) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Todo item not found' });
-      }
-
-      return res.status(200).json({
-        success: true,
-        data: updatedItem,
-        message: 'Todo item was updated',
-      });
-    })
-
-    .catch((err) => res.status(400).json({ success: false, error: err }));
 };
 
-const createTodo = async (req, res) => {
+const createTodo = async (id, body) => {
   // post todo item to mongoDB
-  const { body } = req;
-  // console.log(body.title);
-  if (!body) {
-    return res
-      .status(400)
-      .json({ success: false, error: 'You must provide a todo item' });
-  }
-  const todoItem = await new Todo({
-    title: body.title.title,
-    body: body.title.body,
-    userId: req.user._id,
-  });
+  if (!body) return { success: false, error: 'You must provide a todo item' };
 
-  todoItem
-    .save()
-    .then(() => res.status(200).json({
+  try {
+    const todoItem = await new Todo({
+      title: body.title.title,
+      body: body.title.body,
+      userId: id,
+    });
+    todoItem
+      .save();
+    return {
       success: true,
       id: todoItem._id,
       message: 'Todo item created',
       data: todoItem,
-    }))
-    .catch((err) => res.status(400).json({ error: err, message: 'Todo item not created' }));
+    };
+  } catch (error) {
+    return { success: false, error, message: 'Todo item not created' };
+  }
 };
 
-const deleteTodoItem = async (req, res) => {
+const deleteTodoItem = async (id) => {
   // find single todo by ID and delete from mongoDB
-  await Todo.findOneAndDelete({ _id: req.params.id })
-    .exec()
-    .then((todo) => {
-      if (!todo) {
-        return res
-          .status(404)
-          .json({ success: false, error: 'Todo item not found' });
-      }
-      return res
-        .status(200)
-        .json({ success: true, message: 'Todo item deleted' });
-    })
-    .catch((err) => res.status(400).json({ success: false, error: err }));
+  try {
+    await Todo.findOneAndDelete({ _id: id })
+      .exec();
+    return { success: true, message: 'Todo item deleted' };
+  } catch (error) {
+    return { success: false, error };
+  }
 };
 
 module.exports = {
--- todo-server/routes/todoRoutes.js
+++ todo-server/routes/todoRoutes.js
@@ -1,19 +1,49 @@
+/* eslint-disable no-underscore-dangle */
 const express = require('express');
 const todoCtrl = require('../controllers/todoController');
 
 const requireAuth = require('../middlewares/requireAuth');
 
 const router = express.Router();
-// router.use(requireAuth);
 
-router.get('/api/todo-list', requireAuth, todoCtrl.getTodoList);
+router.get('/api/todo-list', requireAuth, async (req, res) => {
+  const result = await todoCtrl.getTodoList(req.user._id);
+  if (result.success) {
+    return res.status(200).json(result);
+  }
+  return res.status(404).json(result);
+});
 
-router.get('/api/todo-list/:id', requireAuth, todoCtrl.getTodoById);
+router.get('/api/todo-list/:id', requireAuth, async (req, res) => {
+  const result = await todoCtrl.getTodoById(req.params.id);
+  if (result.success) {
+    return res.status(200).json(result);
+  }
+  return res.status(400).json(result);
+});
 
-router.post('/api/todo-list', requireAuth, todoCtrl.createTodo);
+router.post('/api/todo-list', requireAuth, async (req, res) => {
+  const result = await todoCtrl.createTodo(req.user._id, req.body);
+  if (result.success) {
+    return res.status(200).json(result);
+  }
+  return res.status(400).json(result);
+});
 
-router.put('/api/todo-list/:id', requireAuth, todoCtrl.updateTodoItem);
+router.put('/api/todo-list/:id', requireAuth, async (req, res) => {
+  const result = await todoCtrl.updateTodoItem(req.params.id, req.body);
+  if (result.success) {
+    return res.status(200).json(result);
+  }
+  return res.status(400).json(result);
+});
 
-router.delete('/api/todo-list/:id', requireAuth, todoCtrl.deleteTodoItem);
+router.delete('/api/todo-list/:id', requireAuth, async (req, res) => {
+  const result = await todoCtrl.deleteTodoItem(req.params.id);
+  if (result.success) {
+    return res.status(200).json(result);
+  }
+  return res.status(400).json(result);
+});
 
 module.exports = router;
