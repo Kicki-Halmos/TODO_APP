444c5f30f1d0a045712121f1c567e911d0932965
Kicki Halmos <kickihalmos@gmail.com>
2021-11-02 11:20:57 +0100

    add a final test to server

--- todo-server/_tests_/index.test.js
+++ todo-server/_tests_/index.test.js
@@ -6,10 +6,24 @@
 const request = require('supertest');
 const app = require('../index');
 
+let token;
+
+test('that loginrequest returns a token', async () => {
+  const response = await request(app)
+    .post('/api/login')
+    .send({ email: "rosa@test.se", password: "oliver" })
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/);
+  const { statusCode } = response;
+  expect(statusCode).toBe(200);
+  token = response.body.data.token;
+  expect(token).not.toBe(undefined);
+});
+
 test('that request returns 200 with authentication header set', async () => {
   const response = await request(app)
     .get('/api/todo-list')
-    .set('Authorization', 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTdmZTU1ZmNjZTJkZjA2NTYzOGNhMDUiLCJpYXQiOjE2MzU3NzUzODh9.j8gsQiBFL_yksI9XhHkX3hMmJ7YPVlKEIfqk_bMC83s');
+    .set('Authorization', `Bearer ${token}`);
   const { statusCode } = response;
   expect(statusCode).toBe(200);
 });
